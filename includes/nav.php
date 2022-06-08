7554dad9c8ee4f1e1215708c4276b4f3be92519e
BouazziH <bouazzicdi@gmail.com>
2022-06-08 09:29:47 +0200

    travail a fred

--- includes/nav.php
+++ includes/nav.php
@@ -2,6 +2,9 @@
     <nav>
         <ul>
             <li><a href="index.php">Accueil</a></li>
+            <li><a href="contact.inc.php">Contact</a></li>
+            <li><a href="inscription.inc.php">Inscription</a></li>
+            <li><a href="connection.inc.php">Connection</a></li>
         </ul>
     </nav>
 </header>
