fae0d532589b4581e37bfc11e94f314c700b4174
BouazziH <bouazzicdi@gmail.com>
2022-06-22 11:24:08 +0200

    definition interfaces utilisateur

---
+++ classes/Utilisateur.interface.php
@@ -0,0 +1,12 @@
+<?php
+
+interface Utilisateur
+{
+
+    public function inscrireUtilisateur();
+    public function modifierUtilisateur();
+    public function supprimerUtilisateur();
+    public function connceterUtilisateur();
+    public function deconnecterUtilisateur();
+    
+}
